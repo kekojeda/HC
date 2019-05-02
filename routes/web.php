032026d6a592f8c93114d6cdd833d261018932c9
KeKo <kekojeda@gmail.com>
2019-05-02 15:34:48 -0300

    Update de config

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcomes');
+    return view('welcome');
 });
+
+Auth::routes(['verify' => true]);
+
+Route::get('/home', 'HomeController@index')->name('home');
