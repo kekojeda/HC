a2a4f2cc238818d8787803028030eb1df731e52a
KeKo <kekojeda@gmail.com>
2019-05-03 12:05:22 -0300

    Commit modelo Roles con relaciones

--- app/Role.php
+++ app/Role.php
@@ -6,5 +6,36 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'slug', 'description'
+    ];
+
+//RELACIONES
+    public function permissions() // permissions en plural porque un rol tiene muchos permisos
+    {
+        return $this-> hasMany('App/Permission');
+    }
+
+    public function users()
+    {
+        return $this->belongToMany('App/User');
+    }
+
+
+
+
+
+//ALMACENAMIENTO
+
+
+//VALIDACION
+
+
+//RECUPERACION DE INFORMACION
+
+
+//OTRAS OPERACIONES
+
+
+
 }
